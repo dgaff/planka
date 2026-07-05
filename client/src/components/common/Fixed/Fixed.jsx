5b3350459000ea8ab04dbf819ebcf2bd06887e54
Doug Gaff <doug.gaff@gmail.com>
2026-07-05 16:18:51 -0400

    fixed mobile styling for custom filter groups

--- client/src/components/common/Fixed/Fixed.jsx
+++ client/src/components/common/Fixed/Fixed.jsx
@@ -3,7 +3,7 @@
  * Licensed under the Fair Use License: https://github.com/plankanban/planka/blob/master/LICENSE.md
  */
 
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 
 import selectors from '../../../selectors';
@@ -16,12 +16,34 @@ import BoardActions from '../../boards/BoardActions';
 
 import styles from './Fixed.module.scss';
 
+const CSS_VAR = '--fixed-height';
+
 const Fixed = React.memo(() => {
   const { projectId } = useSelector(selectors.selectPath);
   const board = useSelector(selectors.selectCurrentBoard);
 
+  const wrapperRef = useRef(null);
+
+  // Expose the real height of the fixed header as a CSS variable so the board
+  // content below can offset itself correctly. The header wraps onto extra rows
+  // on narrow screens (e.g. many custom filters on mobile), so a static offset
+  // would let the content slide underneath it.
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver(() => {
+      const height = wrapperRef.current ? wrapperRef.current.offsetHeight : 0;
+      document.documentElement.style.setProperty(CSS_VAR, `${height}px`);
+    });
+
+    resizeObserver.observe(wrapperRef.current);
+
+    return () => {
+      resizeObserver.disconnect();
+      document.documentElement.style.removeProperty(CSS_VAR);
+    };
+  }, []);
+
   return (
-    <div className={styles.wrapper}>
+    <div ref={wrapperRef} className={styles.wrapper}>
       <Header />
       <PromoBanner />
       <Favorites />
